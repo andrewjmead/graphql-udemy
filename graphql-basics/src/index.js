11b772393935be37b70aa7f1cf6ea687ea01816e
thomas <dev@terickson.io>
2019-09-12 16:54:58 -0600

    added nodemon

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,42 @@
-console.log('hello graphql');
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+  }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        title() {
+            return 'low beams'
+        },
+        price() {
+            return 2.33
+        },
+        releaseYear() {
+            return null
+        },
+        rating() {
+            return 9.00
+        },
+        inStock() {
+            return true;
+        }
+    },   
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('server is up')
+})
