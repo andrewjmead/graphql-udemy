52301b68ad525d9b1d1ccc6f7bf6b05673895341
thomas <dev@terickson.io>
2019-09-17 14:48:51 -0600

    updated

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,22 +1,75 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+
+const users = [
+    { id: 1, name: 'yossarian', job: 'bombardier'},
+    { id: 2, name: 'milo minderbinder', job: 'cook and contractor'},
+    { id: 3, name: 'nately', job: 'chestnut hoarder'}
+]
+
+const posts = [
+    {id: 11, title: 'How to perfect the aeropress', author: 'blue bottle coffee', published: true},
+    {id: 12, title: 'How to make perfect Hario V60', author: 'stumptown coffee', published: false},
+    {id: 13, title: 'How to use the french press', author: 'spur coffee', published: false}
+]
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    sumTotal(numbers: [Float!]!): Float!
+    users(query: String): [User!]!
+    me: User!
+    posts(specificPost: String): [Post]!
+  }
+
+  type User {
+      id: Int!,
+      name: String!,
+      age: Int,
+      job: String
+  }
+
+  type Post {
+      id: Int!,
+      title: String!,
+      author: String!,
+      published: Boolean
   }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        sumTotal(paretn, args, ctx, info){
-            
-            var x = args.numbers.reduce((acc, elem) => {
-                return acc+elem
-            })
-            console.log(x)
-            return x;
+        me() {
+            return {
+                id: 1234,
+                name: 'tom',
+                age: 32,
+                job: 'builder'
+            }
+        },
+        posts(partent, args, ctx, info) {
+            let aPost;
+            if(args.specificPost) {
+                aPost = posts.filter((post) => {
+                    return post.title.toLowerCase().includes(args.specificPost.toLowerCase())
+                })
+            }
+            else {
+                aPost = posts;
+            }
+            return aPost;
+        },
+        users(partent, args, ctx, info) {
+            let theUsers;
+            if(args && args.query) {
+                theUsers = users.filter((element) => {
+                    return element.name.toLowerCase().includes(args.query.toLowerCase())
+                });
+            }
+            else{
+                theUsers = users;
+            }
+            return theUsers;
+
         }
     }
 }
