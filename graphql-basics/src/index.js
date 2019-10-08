3b3a1073f63e4f903f2b5fd7ac7c4a0f76144862
erickstm87 <dev@terickson.io>
2019-10-07 21:46:06 -0600

    struggling with author field

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,20 +1,24 @@
 import { GraphQLServer } from 'graphql-yoga';
+import uuidv4 from 'uuid/v4';
 
 const comments = [
     {
         id: 12345,
         text: 'It does indeed take great skill thanks blue bottle',
-        author: 'blue bottle'
+        author: 'blue bottle',
+        post: 123
     },
     {
         id: 1234,
         text: 'Aeropress is simply the best thanks Stumptown you guys are kind of hipster dummies',
-        author: 'Stumptown'
+        author: 'Stumptown',
+        post: 666
     },
     {
         id: 123,
         text: 'Not bad, mostly use while camping cause it\'s so easy thanks Coffee guy',
-        author: 'Coffee guy'
+        author: 'Coffee guy',
+        post: 6666
     }
 ]
 
@@ -23,19 +27,22 @@ const posts = [
         title: 'how to brew hario V60',
         body: 'with great skill',
         author: 'blue bottle',
-        published: false
+        published: false,
+        id: 123
     },
     {
         title: 'how to brew aeropress',
         body: 'arguable the best brew you\'ll  find however not everyone will agree with this',
         author: 'Stumptown',
-        published: true
+        published: true,
+        id: 666
     },
     {
         title: 'how to brew french press',
         body: 'Not too complicated. gives some of that good ole sludge',
         author: 'Coffee guy',
-        published: true
+        published: true,
+        id: 6666
     }
 ];
 
@@ -61,6 +68,11 @@ const users = [
 
 // Type definitions (schema)
 const typeDefs = `
+  type Mutation {
+    createUser(name: String!, email: String!, age: Int): User!,
+    createPost(title: String!, body: String!, published: Boolean!, name: String!): Post!
+  },
+
   type Query {
     users: [User!]!
     me: User!
@@ -70,10 +82,11 @@ const typeDefs = `
   },
 
   type User {
-    id: Int!,
+    id: ID!,
     name: String!,
     age: Int,
-    occupation: String!  
+    occupation: String!,
+    email: String!,
     posts: [Post!],
     comments: [Comment!]
   },
@@ -83,17 +96,57 @@ const typeDefs = `
       body: String,
       author: User!,
       published: Boolean,
+      id: ID!,
+      comments: [Comment!]
   },
 
   type Comment {
-      id: Int!,
+      id: ID!,
       text: String!,
-      author: User!
+      author: User!,
+      post: [Post!]
   }
 `
 
 // Resolvers
 const resolvers = {
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) => user.email === args.email )
+
+            if(emailTaken) {
+                throw new Error('error already taken')
+            }
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            }
+
+            users.push(user)
+            return user
+        },
+        createPost(parent, args, ctx, info) {
+            const emailUsed = users.some((user) => user.email === args.email );
+            if(!emailUsed) {
+                throw new Error('can\'t do that!!')
+            }
+
+            const post = {
+                id: uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author
+            }
+
+            posts.push(post)
+            return post
+        }
+    },
+
     Query: {
         users(parent, args, ctx, info) {
             return users
@@ -109,12 +162,13 @@ const resolvers = {
             return post
         },
         posts(parent, args, ctx, info) {
-            if(!args) {
-                return posts
-            }
-            return posts.filter((post) => {
-                return post.title.toLowerCase().includes(args.query)
-            });
+            // if(!args) {
+            //     return posts
+            // }
+            // return posts.filter((post) => {
+            //     return post.title.toLowerCase().includes(args.query)
+            // });
+            return posts
         },
         comments(parent, args, ctx, info) {
             return comments
@@ -128,6 +182,11 @@ const resolvers = {
             return users.find((user) => {
                 return user.name === parent.author
             })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => 
+                comment.post === parent.id
+            )
         }
     },
     User: {
@@ -146,6 +205,11 @@ const resolvers = {
         author(parent, args, ctx, info) {
             return users.find((user) => 
                 user.name === parent.author
+            )
+        },
+        post(parent, args, ctx, info) {
+            return posts.filter((post) => 
+                post.id === parent.post
             )
         }
     }
